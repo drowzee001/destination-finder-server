55798068cbae03ecb889da5521e6df40da401409
Donovan Rowzee <44952478+drowzee001@users.noreply.github.com>
2021-07-19 23:11:07 -0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,10 +6,12 @@ const User = require("../models/User");
 
 module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
-  console.log(token)
+  console.log("token+" + token);
   // Check for token
-  if (!token)
+  if (!token) {
+    console.log("token check =" + token);
     return res.status(401).json({ msg: "No token, authorization denied" });
+  }
   try {
     // Verify token
     const decoded = jwt.verify(token, process.env.SECRET);
