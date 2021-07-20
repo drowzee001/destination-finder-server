7e8dce5093b83bf720eb1c60d94dbb18163a47f7
Donovan Rowzee <44952478+drowzee001@users.noreply.github.com>
2021-07-20 11:47:42 -0700

    Removed client static files codee

--- index.js
+++ index.js
@@ -31,15 +31,5 @@ mongoose
 app.use("/users", require("./routes/users"));
 app.use("/locations", require("./routes/locations"));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Serve listening on port ${port}!`));
