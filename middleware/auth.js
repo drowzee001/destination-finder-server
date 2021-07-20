bc17959ec4fadd38b45b5a7cdcf39ccde923146a
Donovan Rowzee <44952478+drowzee001@users.noreply.github.com>
2021-07-20 10:32:34 -0700

    Fixed http headers error

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
   // Check for token
   if (!token) {
-    res.status(401).json({ msg: "No token, authorization denied" });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
   try {
     // Verify token
