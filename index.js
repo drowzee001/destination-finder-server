fa81a12692f156c79ff65c8b4b07ac800f8278fb
Donovan Rowzee <44952478+drowzee001@users.noreply.github.com>
2021-07-21 11:06:59 -0700

    Updated CORS

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://main.d3oiu7p7egscew.amplifyapp.com",
+    origin: "https://destination-finder.donovanrowzee.net",
     credentials: true,
   })
 );
